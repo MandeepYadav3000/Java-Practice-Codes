114dedc3b350686d7f59c28a327a7d0dd0cb6811
MandeepYadav3000 <mandeepyadav3000@gmail.com>
2022-11-06 07:29:38 +0530

    Java Examples

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int x,y,z,m;
+        x = 42;
+        y = x++;
+
+        z = 43;
+        m = ++z;
+
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        System.out.println("z = " + z);
+        System.out.println("m = " + m);
+
+    }
+}
